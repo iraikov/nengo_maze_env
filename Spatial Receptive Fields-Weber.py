708dad931a69a7d1cd67d9c3f62d4a8c525337ff
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-08-05 07:09:06 -0700

    updated inhibitory plasticity rule; added hebbian plasticity rule and initial implementation of weber paper

---
+++ Spatial Receptive Fields-Weber.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+from functools import partial
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import nengo
+
+import numpy as np
+import scipy.interpolate
+from scipy.interpolate import Rbf, CubicSpline
+from rbf.pde.nodes import disperse, poisson_disc_nodes
+from hsp import HSP
+from isp import ISP
+
+from nengo_extras.plot_spikes import (
+    cluster, merge, plot_spikes, preprocess_spikes, sample_by_variance, sample_by_activity)
+from nengo_extras.neurons import (
+    rates_kernel, rates_isi )
+
+# In[2]:
+
+
+def generate_linear_trajectory(trajectory, temporal_resolution=1., velocity=30., equilibration_duration=None):
+    """
+    Construct coordinate arrays for a spatial trajectory, considering run velocity to interpolate at the specified
+    temporal resolution. Optionally, the trajectory can be prepended with extra distance traveled for a specified
+    network equilibration time, with the intention that the user discards spikes generated during this period before
+    analysis.
+
+    :param trajectory: namedtuple
+    :param temporal_resolution: float (s)
+    :param equilibration_duration: float (s)
+    :return: tuple of array
+    """
+        
+    velocity = velocity  # (cm / s)
+    spatial_resolution = velocity * temporal_resolution
+    x = trajectory[:, 0]
+    y = trajectory[:, 1]
+    
+    if equilibration_duration is not None:
+        equilibration_distance = velocity / equilibration_duration
+        x = np.insert(x, 0, x[0] - equilibration_distance)
+        y = np.insert(y, 0, y[0])
+    else:
+        equilibration_duration = 0.
+        equilibration_distance = 0.
+    
+    segment_lengths = np.sqrt((np.diff(x) ** 2. + np.diff(y) ** 2.))
+    distance = np.insert(np.cumsum(segment_lengths), 0, 0.)
+    
+    interp_distance = np.arange(distance.min(), distance.max() + spatial_resolution / 2., spatial_resolution)
+    interp_x = np.interp(interp_distance, distance, x)
+    interp_y = np.interp(interp_distance, distance, y)
+    t = interp_distance / velocity  # s
+    
+    t = np.subtract(t, equilibration_duration)
+    interp_distance -= equilibration_distance
+    
+    return t, interp_x, interp_y, interp_distance
+
+
+
+# In[3]:
+
+
+arena_dimension = 200
+vert = np.array([[-arena_dimension,-arena_dimension],[-arena_dimension,arena_dimension],
+                 [arena_dimension,arena_dimension],[arena_dimension,-arena_dimension]])
+smp = np.array([[0,1],[1,2],[2,3],[3,0]])
+    
+arena_res = 5
+arena_x = np.arange(-arena_dimension, arena_dimension, arena_res)
+arena_y = np.arange(-arena_dimension, arena_dimension, arena_res)
+
+arena_xx, arena_yy = np.meshgrid(arena_x, arena_y, indexing='ij')
+peak_rate = 1.
+nmodules = 4
+
+field_width_params = [35.0, 0.32]
+exc_field_width  = lambda x: 40. + field_width_params[0] * (np.exp(x / field_width_params[1]) - 1.)
+inh_field_width  = lambda x: 100. + field_width_params[0] * (np.exp(x / field_width_params[1]) - 1.)
+
+exc_module_field_width_dict = {i : exc_field_width( float(i) / float(nmodules) ) for i in range(nmodules)}
+inh_module_field_width_dict = {i : inh_field_width( float(i) / float(nmodules) ) for i in range(2)}
+    
+
+def generate_input_rates(module_field_width_dict, basis_function='gaussian', spacing_factor=1.0, peak_rate=1.):
+    input_nodes_dict = {}
+    input_groups_dict = {}
+    input_rates_dict = {}
+    
+    for m in module_field_width_dict:
+        nodes, groups, _ = poisson_disc_nodes(module_field_width_dict[m], (vert, smp))
+        input_groups_dict[m] = groups
+        input_nodes_dict[m] = nodes
+        input_rates_dict[m] = {}
+        vertlst = [[v[0], v[1]] for v in vert]
+        
+        for i in range(nodes.shape[0]):
+            xs = [[nodes[i,0], nodes[i,1]]] + vertlst
+            x_obs = np.asarray(xs).reshape((1,-1))
+            u_obs = np.asarray([[peak_rate]]).reshape((1,-1))
+            input_rate_ip  = Rbf(x_obs[:,0], x_obs[:,1], u_obs,
+                                 function=basis_function, 
+                                 epsilon=module_field_width_dict[m] * spacing_factor)
+            input_rates_dict[m][i] = input_rate_ip
+            
+    return input_nodes_dict, input_groups_dict, input_rates_dict
+
+exc_input_nodes_dict, exc_input_groups_dict, exc_input_rates_dict = \
+    generate_input_rates(exc_module_field_width_dict, spacing_factor=0.5, peak_rate=peak_rate)
+inh_input_nodes_dict, inh_input_groups_dict, inh_input_rates_dict = \
+    generate_input_rates(inh_module_field_width_dict, spacing_factor=1.0, peak_rate=peak_rate)
+
+def make_input_rate_matrix(input_rates_dict):
+
+    input_rate_maps = []
+    for m in input_rates_dict:
+        for i in range(len(input_rates_dict[m])):
+            input_rates = exc_input_rates_dict[m][i](arena_xx, arena_yy)
+            input_rate_maps.append(input_rates.ravel())
+    input_rate_matrix = np.column_stack(input_rate_maps)
+
+    return input_rate_matrix
+
+#exc_input_rate_matrix = make_input_rate_matrix(exc_input_rates_dict)
+#inh_input_rate_matrix = make_input_rate_matrix(inh_input_rates_dict)
+
+def plot_input_rates(input_rates_dict):
+    for m in input_rates_dict:
+        plt.figure()
+        arena_map = np.zeros(arena_xx.shape)
+        for i in range(len(input_rates_dict[m])):
+            input_rates = input_rates_dict[m][i](arena_xx, arena_yy)
+            arena_map += input_rates
+        plt.pcolor(arena_xx, arena_yy, arena_map, cmap=cm.jet)
+        ax = plt.gca()
+        ax.set_aspect('equal')
+        plt.show()
+        
+plot_input_rates(exc_input_rates_dict)
+plot_input_rates(inh_input_rates_dict)
+
+
+# In[4]:
+
+
+diag_trajectory = np.asarray([[-100, -100], [100, 100]])
+trj_t, trj_x, trj_y, trj_d = generate_linear_trajectory(diag_trajectory, temporal_resolution=0.001)
+    
+exc_trajectory_input_rates = { m: {} for m in exc_input_rates_dict }
+inh_trajectory_input_rates = { m: {} for m in inh_input_rates_dict }
+exc_trajectory_inputs = []
+inh_trajectory_inputs = []
+    
+for m in exc_input_rates_dict:
+    for i in exc_input_rates_dict[m]:
+        input_rates = exc_input_rates_dict[m][i](trj_x, trj_y)
+        input_rates[np.isclose(input_rates, 0., atol=1e-4, rtol=1e-4)] = 0.
+        exc_trajectory_input_rates[m][i] = input_rates
+        input_rates_ip = CubicSpline(trj_t, input_rates)
+        exc_trajectory_inputs.append(input_rates_ip)
+        
+for m in inh_input_rates_dict:
+    for i in inh_input_rates_dict[m]:
+        input_rates = inh_input_rates_dict[m][i](trj_x, trj_y)
+        input_rates[np.isclose(input_rates, 0., atol=1e-4, rtol=1e-4)] = 0.
+        inh_trajectory_input_rates[m][i] = input_rates
+        input_rates_ip = CubicSpline(trj_t, input_rates)
+        inh_trajectory_inputs.append(input_rates_ip)
+            
+
+# In[5]:
+
+
+def trajectory_input(trajectory_inputs, t, centered=False):
+    if centered:
+        result = np.asarray([ 2.*y(t) - 1. for y in trajectory_inputs ])
+    else:
+        result = np.asarray([ y(t) for y in trajectory_inputs ])
+    return result
+   
+#E_train = 2.*exc_input_rate_matrix - 1.
+#I_train = 2.*inh_input_rate_matrix - 1.
+
+
+# In[ ]:
+
+
+def make_weber_srf_model():
+        with nengo.Network(label="Weber spatial receptive field model", seed=19) as model:
+
+            N_Exc = len(exc_trajectory_inputs)
+            N_Inh = len(inh_trajectory_inputs)
+            model.ExcInput = nengo.Node(partial(trajectory_input, exc_trajectory_inputs),
+                                        size_out=N_Exc)
+            model.InhInput = nengo.Node(partial(trajectory_input, inh_trajectory_inputs),
+                                        size_out=N_Inh)
+                                         
+            model.Exc = nengo.Ensemble(N_Exc, dimensions=1,
+                                       neuron_type=nengo.RectifiedLinear(),
+                                       intercepts=nengo.dists.Choice([0.1]),
+                                       max_rates=nengo.dists.Choice([20]))
+            
+            model.Inh = nengo.Ensemble(N_Inh, dimensions=1,
+                                       neuron_type=nengo.RectifiedLinear(),
+                                       intercepts=nengo.dists.Choice([0.1]),
+                                       max_rates=nengo.dists.Choice([20]))
+            
+            model.Output = nengo.Ensemble(1, dimensions=1,
+                                          neuron_type=nengo.LIFRate(),
+                                          intercepts=nengo.dists.Choice([0.1]),
+                                          max_rates=nengo.dists.Choice([20]))
+
+            nengo.Connection(model.ExcInput, model.Exc.neurons, synapse=nengo.Lowpass(0.01),
+                             transform=np.eye(N_Exc))
+            nengo.Connection(model.InhInput, model.Inh.neurons, synapse=nengo.Lowpass(0.01),
+                             transform=np.eye(N_Inh))
+
+            #model.conn_EI = nengo.Connection(model.Exc.neurons, model.Inh.neurons,
+            #                                 transform=np.ones((N_Inh, N_Exc)) * 1e-6,
+            #                                 synapse=nengo.Alpha(0.01))
+            model.conn_I = nengo.Connection(model.Inh.neurons, model.Output.neurons,
+                                           transform=np.random.normal(size=N_Inh).reshape((1, N_Inh)) * -1e-1,
+                                           synapse=nengo.Alpha(0.01), 
+                                           learning_rule_type=ISP(learning_rate=2e-6, rho0=2.))
+            model.conn_E = nengo.Connection(model.Exc.neurons, model.Output.neurons, 
+                                           transform=np.random.normal(size=N_Exc).reshape((1, N_Exc)) * 5e-2,
+                                           synapse=nengo.Alpha(0.01),
+                                           learning_rule_type=HSP(learning_rate=2e-6))
+                             
+            return model
+
+      
+srf_model = make_weber_srf_model()
+
+with srf_model:
+    p_output_rates = nengo.Probe(srf_model.Output.neurons, 'rates')
+    p_inh_rates = nengo.Probe(srf_model.Inh.neurons, 'rates')
+    p_inh_weights = nengo.Probe(srf_model.conn_I, 'weights')
+    p_exc_rates = nengo.Probe(srf_model.Exc.neurons, 'rates')
+    p_exc_weights = nengo.Probe(srf_model.conn_E, 'weights')
+        
+with nengo.Simulator(srf_model) as sim:
+    sim.run(np.max(trj_t))
+    
+#plot_spikes(sim.trange(), sim.data[p_inh_rates][0,:])
+
+
+
+
--- hsp.py
+++ hsp.py
@@ -11,17 +11,16 @@ from nengo.builder.operator import DotInc, ElementwiseInc, Copy, Reset
 from nengo.connection import LearningRule
 from nengo.ensemble import Ensemble, Neurons
 
-# Creates new learning rule for inhibitory plasticity (ISP).
+# Creates new learning rule for Hebbian synaptic plasticity (HSP).
 # Based on the paper:
 #
-#    Inhibitory Plasticity Balances Excitation and Inhibition in Sensory
-#    Pathways and Memory Networks.
-#    T. P. Vogels, H. Sprekeler, F. Zenke, C. Clopath, W. Gerstner.
-#    Science 334, 2011
+# Learning place cells, grid cells and invariances with excitatory and
+# inhibitory plasticity.  Simon Nikolaus Weber, Henning Sprekeler.
+# Elife 2018;7:e34560.
 # 
 
-class ISP(LearningRuleType):
-    """Inhibitory plasticity learning rule.  Modifies connection weights
+class HSP(LearningRuleType):
+    """Hebbian synaptic plasticity learning rule.  Modifies connection weights
     according to the presynaptic and postsynaptic firing rates and the
     target firing rate.
 
@@ -30,18 +29,15 @@ class ISP(LearningRuleType):
     probeable = ('pre_filtered', 'post_filtered', 'delta')
     
     learning_rate = NumberParam("learning_rate", low=0, readonly=True, default=1e-6)
-    rho0 = NumberParam("rho0", low=0, readonly=True, default=10.)
-    pre_synapse = SynapseParam("pre_synapse", default=Lowpass(tau=0.01), readonly=True)
+    pre_synapse = SynapseParam("pre_synapse", default=Lowpass(tau=0.005), readonly=True)
     post_synapse = SynapseParam("post_synapse", default=None, readonly=True)
 
     def __init__(self,
                  learning_rate=Default,
-                 rho0=Default,
                  pre_synapse=Default,
                  post_synapse=Default,
                  theta_synapse=Default):
         super().__init__(learning_rate, size_in=0)
-        self.rho0 = rho0
         self.pre_synapse = pre_synapse
         self.post_synapse = (
             self.pre_synapse if post_synapse is Default else post_synapse
@@ -52,25 +48,22 @@ class ISP(LearningRuleType):
         return _remove_default_post_synapse(super()._argreprs, self.pre_synapse)
 
 
-# Builders for ISP
-class SimISP(Operator):
-    r"""Calculate connection weight change according to the inhibitory plasticity rule.
+# Builders for HSP
+class SimHSP(Operator):
+    r"""Calculate connection weight change according to the Hebbian plasticity rule.
     Implements the learning rule of the form:
-    .. math:: \delta{} weight_{ij} = \kappa * (pre * post - \rho_0 * pre)
+    .. math:: \delta{} weight_{ij} = \kappa * pre * post
     where
     * :math:`\kappa` is a scalar learning rate
     * :math:`\weight_{ij}` is the connection weight between the two neurons.
     * :math:`a_i` is the activity of the presynaptic neuron.
     * :math:`a_j` is the firing rate of the postsynaptic neuron.
-    * :math:`\rho_{0}` is the target firing rate
     Parameters
     ----------
     pre_filtered : Signal
         The presynaptic activity, :math:`a_i`.
     post_filtered : Signal
         The postsynaptic activity, :math:`a_j`.
-    rho0 : float
-        The target firing rate, :math:`\rho_{0}`.
     delta : Signal
         The synaptic weight change to be applied, :math:`\Delta \weight_{ij}`.
     learning_rate : float
@@ -83,8 +76,6 @@ class SimISP(Operator):
         The presynaptic activity, :math:`a_i`.
     post_filtered : Signal
         The postsynaptic activity, :math:`a_j`.
-    rho0 : float
-        The target firing rate, :math:`\rho_{0}`.
     delta : Signal
         The synaptic weight change to be applied, :math:`\Delta \weight_{ij}`.
     learning_rate : float
@@ -99,11 +90,9 @@ class SimISP(Operator):
     4. updates ``[delta]``
     """
 
-    def __init__(self, pre_filtered, post_filtered, weights, rho0, delta, learning_rate, tag=None):
-        super(SimISP, self).__init__(tag=tag)
+    def __init__(self, pre_filtered, post_filtered, weights, delta, learning_rate, tag=None):
+        super(SimHSP, self).__init__(tag=tag)
         self.learning_rate = learning_rate
-        self.rho0 = rho0
-        
         self.sets = []
         self.incs = []
         self.reads = [pre_filtered, post_filtered, weights]
@@ -137,53 +126,49 @@ class SimISP(Operator):
     def make_step(self, signals, dt, rng):
         pre_filtered = signals[self.pre_filtered]
         post_filtered = signals[self.post_filtered]
-        weights = signals[self.weights]
         delta = signals[self.delta]
         kappa = self.learning_rate * dt
-        rho0 = self.rho0
+        weights = signals[self.weights]
 
-        def step_simisp():
-            delta[...] = -kappa * pre_filtered * (post_filtered - rho0)
-            delta_sum = np.add(delta, weights)
-            pos = np.argwhere(np.logical_or(delta_sum.flat >= 0, delta_sum.flat < -1)).flat
-            delta[:,pos] = 0.
-        return step_simisp
+        def step_simhsp():
+            factor = 1.0 - (np.dot(weights, weights.T) / np.linalg.norm(weights))
+            delta[...] = kappa * factor * pre_filtered * post_filtered
+        return step_simhsp
 
     
-@Builder.register(ISP)
-def build_isp(model, isp, rule):
-    """Builds a `.ISP` object into a model.
+@Builder.register(HSP)
+def build_hsp(model, hsp, rule):
+    """Builds a `.HSP` object into a model.
     Calls synapse build functions to filter the pre and post activities,
-    and adds a `.SimISP` operator to the model to calculate the delta.
+    and adds a `.SimHSP` operator to the model to calculate the delta.
     Parameters
     ----------
     model : Model
         The model to build into.
-    isp : ISP
+    hsp : HSP
         Learning rule type to build.
     rule : LearningRule
         The learning rule object corresponding to the neuron type.
     Notes
     -----
     Does not modify ``model.params[]`` and can therefore be called
-    more than once with the same `.ISP` instance.
+    more than once with the same `.HSP` instance.
     """
 
     conn = rule.connection
     pre_activities = model.sig[get_pre_ens(conn).neurons]["out"]
     post_activities = model.sig[get_post_ens(conn).neurons]["out"]
-    pre_filtered = build_or_passthrough(model, isp.pre_synapse, pre_activities)
-    post_filtered = build_or_passthrough(model, isp.post_synapse, post_activities)
+    pre_filtered = build_or_passthrough(model, hsp.pre_synapse, pre_activities)
+    post_filtered = build_or_passthrough(model, hsp.post_synapse, post_activities)
     weights = model.sig[conn]["weights"]
-    
+
     model.add_op(
-        SimISP(
+        SimHSP(
             pre_filtered,
             post_filtered,
             weights,
-            isp.rho0,
             model.sig[rule]["delta"],
-            learning_rate=isp.learning_rate,
+            learning_rate=hsp.learning_rate,
         )
     )
 
