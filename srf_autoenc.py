07c55acf450a95f8d8de5a333000e2c6a92991c9
Ivan Raikov <ivan.g.raikov@gmail.com>
2020-11-09 11:16:01 -0800

    refactoring srf autoencoder model into functions

--- srf_autoenc.py
+++ srf_autoenc.py
@@ -17,48 +17,56 @@ import tensorflow as tf
 from vlae import flatten_binary_crossentropy 
 # In[2]:
 
-
 xdim = 128
 ydim = 96
 
-coords_data = []
-time_data = []
-with open("./rgbd_dataset_freiburg1_xyz/groundtruth.txt", 'r') as f:
-    for line in f.readlines():
-        if line.startswith('#'):
-            continue
-        items = line.split(' ')
-        t = float(items[0])
-        tx, ty, tz = map(float, items[1:4])
-        time_data.append(t)
-        coords_data.append([tx, ty, tz])
+default_data_prefix = './rgbd_dataset_freiburg1_xyz'
 
-frame_time_data = []
-with open("./rgbd_dataset_freiburg1_xyz/rgb.txt", 'r') as f:
-    for line in f.readlines():
-        if line.startswith('#'):
-            continue
-        items = line.split(' ')
-        t = float(items[0])
-        frame_time_data.append(t)
+def prepare_input_data(data_prefix, n_sample=-1):
 
-input_time_array = np.asarray(frame_time_data)        
-input_time_array = (input_time_array - input_time_array[0])
+    coords_data = []
+    time_data = []
+    with open("%s/groundtruth.txt" % data_prefix, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            items = line.split(' ')
+            t = float(items[0])
+            tx, ty, tz = map(float, items[1:4])
+            time_data.append(t)
+            coords_data.append([tx, ty, tz])
 
-list_data = glob.glob("./rgbd_dataset_freiburg1_xyz/rgb/*.png")
+    frame_time_data = []
+    with open("%s/rgb.txt" % data_prefix, 'r') as f:
+        for line in f.readlines():
+            if line.startswith('#'):
+                continue
+            items = line.split(' ')
+            t = float(items[0])
+            frame_time_data.append(t)
 
-n_input = len(list_data)
-all_data = np.zeros((n_input,xdim,ydim))
-for i in range(n_input):
-    data_frame = np.array(Image.open(list_data[i]).resize((xdim,ydim)).convert('LA'))/255.
-    all_data[i,:,:] = data_frame[:,:,0].T
+    input_time_array = np.asarray(frame_time_data)        
+    input_time_array = (input_time_array - input_time_array[0])
 
-vlae = tf.keras.models.load_model("vlae_dataset_freiburg1",
-                                  custom_objects={ 'flatten_binary_crossentropy': flatten_binary_crossentropy })
+    input_file_list = glob.glob("%s/rgb/*.png" % data_prefix)
+    
+    n_input = len(input_file_list)
+    all_data = np.zeros((n_input,xdim,ydim))
+    for i in range(n_input):
+        data_frame = np.array(Image.open(input_file_list[i]).resize((xdim,ydim)).convert('LA'))/255.
+        all_data[i,:,:] = data_frame[:,:,0].T
 
-n_sample = 400
-encoder = vlae.get_layer("encoder")
-encoded_inputs = encoder.predict(all_data[:n_sample])
+    vlae = tf.keras.models.load_model("vlae_dataset_freiburg1",
+                                      custom_objects={ 'flatten_binary_crossentropy': flatten_binary_crossentropy })
+
+    encoder = vlae.get_layer("encoder")
+    if n_sample > 0:
+        encoded_inputs = encoder.predict(all_data[:n_sample])
+    else:
+        encoded_inputs = encoder.predict(all_data)
+
+    return input_time_array, all_data, encoded_inputs, encoder
+
 
 def generate_input_ip(time_array, input_arrays, peak_rate=1., basis_function='gaussian', n_trials=1, nsample=None, sample_seed=0, trial_dt=0.001):
 
@@ -123,29 +131,25 @@ def generate_const_input_ip(time_array, n, peak_rate=1., basis_function='inverse
     return input_ip_dict
 
 
-n_trials = 3
-peak_rate = 1.
-n_inh = 200
-print("Generating inputs...")
-exc_input_ip_dict = generate_input_ip(input_time_array[:n_sample], encoded_inputs, peak_rate=peak_rate, n_trials=n_trials)
-#inh_input_ip_dict = generate_input_ip(input_time_array[:n_sample], encoded_inputs, peak_rate=peak_rate, n_trials=n_trials, nsample=n_inh)
+def generate_trajectory_inputs(input_time_array, encoded_inputs, n_trials = 1, peak_rate = 1.):
+                               
+    print("Generating inputs...")
+    n_sample = len(encoded_inputs[0])
+    print("n_sample = %d" % n_sample)
+    exc_input_ip_dict = generate_input_ip(input_time_array[:n_sample], encoded_inputs,
+                                          peak_rate=peak_rate, n_trials=n_trials)
+    
+    exc_trajectory_inputs = []
 
+    t_end = np.max(input_time_array[:n_sample]) * n_trials
 
-exc_trajectory_inputs = []
-#inh_trajectory_inputs = []
+    for m in sorted(exc_input_ip_dict.keys()):
+        for i in sorted(exc_input_ip_dict[m].keys()):
+            input_rates_ip = exc_input_ip_dict[m][i]
+            exc_trajectory_inputs.append(input_rates_ip)
 
-t_end = np.max(input_time_array[:n_sample]) * n_trials
+    return exc_trajectory_inputs
 
-for m in sorted(exc_input_ip_dict.keys()):
-    for i in sorted(exc_input_ip_dict[m].keys()):
-        input_rates_ip = exc_input_ip_dict[m][i]
-        exc_trajectory_inputs.append(input_rates_ip)
-
-#for m in sorted(inh_input_ip_dict.keys()):
-#for i in sorted(inh_input_ip_dict.keys()):
-#    input_rates_ip = inh_input_ip_dict[i]
-#    inh_trajectory_inputs.append(input_rates_ip)
-            
 
 def plot_input_rates(input_rate_ips, t_end, dt=0.001, num=None):
     sim_t = np.arange(0., t_end, dt)
@@ -176,14 +180,11 @@ def autoenc_input(input_ips, t, *args):
     return np.concatenate([on, off])
 
 
-# In[ ]:
+def make_model(exc_trajectory_inputs, N_Outputs = 50, N_Inh = 250, seed=19):
+    
+    N_Exc = len(exc_trajectory_inputs)*2
 
-N_Outputs = 50
-N_Exc = len(exc_trajectory_inputs)*2
-N_Inh = n_inh
-
-seed = 19
-srf_network = PRF(exc_input_func = partial(autoenc_input, exc_trajectory_inputs),
+    srf_network = PRF(exc_input_func = partial(autoenc_input, exc_trajectory_inputs),
                   connect_exc_inh_input = True,
                   n_excitatory = N_Exc,
                   n_inhibitory = N_Inh,
@@ -201,22 +202,38 @@ srf_network = PRF(exc_input_func = partial(autoenc_input, exc_trajectory_inputs)
                   label="Spatial receptive field network",
                   seed=seed)
 
-with srf_network:
-    p_output_spikes = nengo.Probe(srf_network.output.neurons, 'spikes', synapse=None)
-    p_exc_rates = nengo.Probe(srf_network.exc.neurons, 'rates')
-    p_inh_rates = nengo.Probe(srf_network.inh.neurons, 'rates')
-    p_inh_weights = nengo.Probe(srf_network.conn_I, 'weights')
-    p_exc_weights = nengo.Probe(srf_network.conn_E, 'weights')
-    if srf_network.conn_EE is not None:
-        p_rec_weights = nengo.Probe(srf_network.conn_EE, 'weights')
-        
-with nengo.Simulator(srf_network, optimize=True) as sim:
-    sim.run(np.max(t_end))
+    with srf_network:
+        p_output_spikes = nengo.Probe(srf_network.output.neurons, 'spikes', synapse=None)
+        p_exc_rates = nengo.Probe(srf_network.exc.neurons, 'rates')
+        p_inh_rates = nengo.Probe(srf_network.inh.neurons, 'rates')
+        p_inh_weights = nengo.Probe(srf_network.conn_I, 'weights')
+        p_exc_weights = nengo.Probe(srf_network.conn_E, 'weights')
+        if srf_network.conn_EE is not None:
+            p_rec_weights = nengo.Probe(srf_network.conn_EE, 'weights')
+
+    return srf_network
+
+def run(model, t_end):
+    with nengo.Simulator(model, optimize=True) as sim:
+        sim.run(np.max(t_end))
     
-output_spikes = sim.data[p_output_spikes]
-np.save("srf_output_spikes", np.asarray(output_spikes, dtype=np.float32))
-np.save("srf_time_range", np.asarray(sim.trange(), dtype=np.float32))
-output_rates = rates_kernel(sim.trange(), output_spikes, tau=0.1)
+    output_spikes = sim.data[p_output_spikes]
+    np.save("srf_output_spikes", np.asarray(output_spikes, dtype=np.float32))
+    np.save("srf_time_range", np.asarray(sim.trange(), dtype=np.float32))
+    output_rates = rates_kernel(sim.trange(), output_spikes, tau=0.1)
+
+    return sim.trange(), output_spikes, output_rates
+
+if __name__ == '__main__':
+    n_sample = 400
+    input_time_array, all_data, encoded_inputs, encoder  = prepare_input_data(default_data_prefix, n_sample=n_sample)
+    t_end = np.max(input_time_array[:n_sample])
+    exc_trajectory_inputs = generate_trajectory_inputs(input_time_array, encoded_inputs,
+                                                       n_trials = 3, peak_rate = 1.)
+    model = make_model(exc_trajectory_inputs, N_Outputs = 50, N_Inh = 250, seed=19)
+    run(model, t_end)
+    
+    
 #sorted_idxs = np.argsort(-np.argmax(output_rates[53144:].T, axis=1))
 
 #output_rates = sim.data[p_output_rates]
